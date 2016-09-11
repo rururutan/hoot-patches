e85dd01d1db758c06a78b424593f4577b91a33d6
RuRuRu <rururutan+github@gmail.com>
2016-09-11 14:20:26 +0900

    ぱせり(PC-98)を追加

---
+++ pc98/parseri/lime/src/comp.bat
@@ -0,0 +1,1 @@
+nasmw -f bin -opsmus_98.com psmus_98.asm
---
+++ pc98/parseri/lime/src/psmus_98.asm
@@ -0,0 +1,115 @@
+; ぱせりMUSIC.com 演奏
+; メインルーチン (for pc98dos)
+;
+
+%include 'hoot.inc'
+int_hoot	equ	0x7f
+int_driver	equ	0xf2
+
+		ORG	0x0100
+		USE16
+		CPU	186
+
+start:
+		cli
+		mov	ax, cs
+		mov	ds, ax
+		mov	es, ax
+
+		mov	dx, HOOTFUNC
+		mov	al, HF_DISABLE		; 初期化中はhoot呼び出しを禁止
+		out	dx, al
+
+		mov	ax, cs			; スタック設定
+		mov	ss, ax
+		mov	sp, stack
+
+		mov	bx, prgend
+		add	bx, 0x0f
+		shr	bx, 4
+		mov	ah, 0x4a		; AH=4a modify alloc memory(ES:BX)
+		int	0x21
+
+		mov	ah, 0x48		; AH=48 alloc memory
+		mov	bx, 0x1000
+		int	0x21
+		mov	[bufseg],ax
+
+		mov	ah, 0x25		; hootドライバ登録
+		mov	al, int_hoot
+		mov	dx, vect_hoot
+		int	0x21
+
+		mov	dx, HOOTFUNC
+		mov	al, HF_ENABLE		; hoot呼び出しを許可
+		out	dx, al
+		sti
+
+mainloop:
+		mov	ax, 0x9801		; ダミーポーリング
+		int	0x18
+		jmp	short mainloop
+
+; hootからコールされる
+; inp8(HOOTPORT) = 0 → PC98DOS::Play
+; inp8(HOOTPORT) = 2 → PC98DOS::Stop
+; _code = inp8(HOOTPORT+2)～inp8(HOOTPORT+5)
+
+vect_hoot:
+		pusha
+		push	ds
+		push	es
+		mov	ax, cs
+		mov	ds, ax
+		mov	es, ax
+		mov	dx, HOOTPORT
+		in	al, dx
+		cmp	al, HP_PLAY
+		jz	short .play
+		cmp	al, HP_STOP
+		jz	short .fadeout
+.ed:
+		pop	es
+		pop	ds
+		popa
+		iret
+
+.stop:
+.fadeout:
+		mov	ax, 0x01		; 演奏停止
+		int	int_driver
+		jmp	short .ed
+
+.play:
+		mov	ax, 0x01		; 演奏停止
+		int	int_driver
+
+		push	ds
+		mov	ah, 0x3f		; AH=3F file read
+		xor	bx, bx
+		mov	cx, 0xffff
+		lds	dx, [bufofs]
+		int	0x21
+		pop	ds
+		jb	.playerr
+
+		mov	ax, 0x00		; 演奏開始
+		les	bx, [bufofs]
+		int	int_driver
+
+.playerr:
+		jmp	.ed
+
+bufofs:
+		dw	0x0000
+bufseg:
+		dw	0x0000
+
+		align	0x10
+		times 0x80 db 0xff		; スタックエリア
+
+stack:
+
+prgend:
+		ends
+
