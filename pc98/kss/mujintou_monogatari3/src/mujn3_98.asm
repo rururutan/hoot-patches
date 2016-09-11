b2c674ddf5bb83446bec29b448a9dcecfe91c3fb
RuRuRu <rururutan+github@gmail.com>
2016-09-11 13:56:43 +0900

    KSS(PC-98)を追加

---
+++ pc98/kss/mujintou_monogatari3/src/mujn3_98.asm
@@ -0,0 +1,115 @@
+; 無人島物語3 FM26K.COM 演奏
+; メインルーチン (for pc98dos)
+;
+; HOOTPORT + 2~3 : ファイルハンドル番号
+;
+
+%include 'hoot.inc'
+int_hoot	equ	0x7f
+int_driver	equ	0xd2
+
+		ORG	0x0100
+		USE16
+		CPU	186
+
+start:
+		cli
+		mov	ax, cs
+		mov	ds, ax
+		mov	es, ax
+
+		mov	dx, HOOTFUNC
+		mov	al, HF_DISABLE		; 初期化中はhoot呼び出しを禁止
+		out	dx, al
+
+		mov	ax, cs			; スタック設定
+		mov	ss, ax
+		mov	sp, stack
+
+		mov	bx, prgend
+		add	bx, 0x0f
+		shr	bx, 4
+		mov	ah, 0x4a		; AH=4a modify alloc memory(ES:BX)
+		int	0x21
+
+		push	ds
+		mov	ah, 0x05		; get buffer
+		int	int_driver
+		mov	[cs:bufseg],ds
+		mov	[cs:bufofs],dx
+		pop	ds
+
+		mov	ah, 0x25		; hootドライバ登録
+		mov	al, int_hoot
+		mov	dx, vect_hoot
+		int	0x21
+
+		mov	dx, HOOTFUNC
+		mov	al, HF_ENABLE		; hoot呼び出しを許可
+		out	dx, al
+
+		sti
+
+mainloop:
+		mov	ax, 0x9801		; ダミーポーリング
+		int	0x18
+		jmp	short mainloop
+
+; hootからコールされる
+; inp8(HOOTPORT) = 0 → PC98DOS::Play
+; inp8(HOOTPORT) = 2 → PC98DOS::Stop
+; _code = inp8(HOOTPORT+2)～inp8(HOOTPORT+5)
+
+vect_hoot:
+		pusha
+		push	ds
+		push	es
+		mov	ax, cs
+		mov	ds, ax
+		mov	es, ax
+		mov	dx, HOOTPORT
+		in	al, dx
+		cmp	al, HP_PLAY
+		jz	short .play
+		cmp	al, HP_STOP
+		jz	short .fadeout
+.ed:
+		pop	es
+		pop	ds
+		popa
+		iret
+
+.stop:
+.fadeout:
+		mov	ah, 0x01		; stop
+		int	int_driver
+		jmp	short .ed
+
+.play:
+		push	ds
+		mov	ah, 0x3f		; AH=3F file read
+		xor	bx, bx
+		mov	cx, 0xffff
+		lds	dx, [bufofs]
+		int	0x21
+		pop	ds
+		jc	.ed
+
+		mov	ah, 0x00		; play
+		int	int_driver
+
+		jmp	.ed
+
+bufofs:
+		dw	0x0000
+bufseg:
+		dw	0x0000
+
+		align	0x10
+		times 0x100 db 0xff		; スタックエリア
+
+stack:
+
+prgend:
+		ends
+
