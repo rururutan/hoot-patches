4efc9b54f5b73a2ee954194444db82f56b6f182f
RuRuRu <rururutan+github@gmail.com>
2019-02-02 10:42:48 +0900

    [PC-98]  Zark Legend Specialを追加

---
+++ pc98/maxima/zark_legend_special/src/comp.bat
@@ -0,0 +1,2 @@
+set DRIVER=zark_98
+nasm -f bin -l %DRIVER%.lst -o %DRIVER%.com %DRIVER%.asm
---
+++ pc98/maxima/zark_legend_special/src/zark_98.asm
@@ -0,0 +1,92 @@
+; Maxima/Zark Legend Special ALL.COM
+;
+; @autor RuRuRu
+; @date 2019/02/01 1st Release
+;
+
+%include 'hoot.inc'
+int_hoot	equ	0x7f
+int_driver	equ	0xa0
+
+		ORG	0x0100
+		USE16
+		CPU	186
+
+start:
+		cli
+		mov	ax, cs
+		mov	ds, ax
+		mov	es, ax
+
+		mov	dx, HOOTFUNC
+		mov	al, HF_DISABLE		; 初期化中はhoot呼び出しを禁止
+		out	dx, al
+
+		mov	ax, cs
+		mov	ss, ax
+		mov	sp, stack
+
+		mov	bx, prgend
+		add	bx, 0x0f
+		shr	bx, 4
+		mov	ah, 0x4a		; AH=4a modify alloc memory(ES:BX)
+		int	0x21
+
+		mov	ah, 0x25		; hootドライバ登録
+		mov	al, int_hoot
+		mov	dx, vect_hoot
+		int	0x21
+
+		mov	dx, HOOTFUNC
+		mov	al, HF_ENABLE		; hoot呼び出しを許可
+		out	dx, al
+		sti
+
+mainloop:
+		mov	ax, 0x9801		; ダミーポーリング
+		int	0x18
+		jmp	short mainloop
+
+; hootからコールされる
+; inp8(HOOTPORT) = 0 → PC98DOS::Play
+; inp8(HOOTPORT) = 2 → PC98DOS::Stop
+; _code = inp8(HOOTPORT+2)〜inp8(HOOTPORT+5)
+
+vect_hoot:
+		pusha
+		push	ds
+		push	es
+		mov	ax, cs
+		mov	ds, ax
+		mov	es, ax
+		mov	dx, HOOTPORT
+		in	al, dx
+		cmp	al, HP_PLAY
+		jz	short .play
+		cmp	al, HP_STOP
+		jz	short .fadeout
+.ed:
+		pop	es
+		pop	ds
+		popa
+		iret
+	
+.stop:
+.fadeout:
+		mov	ax, 0x64
+		int	int_driver
+		jmp	short .ed
+
+.play:
+		mov	dx,HOOTPORT+2		; 曲番号
+		xor	ax,ax
+		in	al,dx
+		int	int_driver
+		jmp	.ed
+
+		align	0x10
+		times 0x100 db 0xff		; スタックエリア
+stack:
+prgend:
+		ends
+
